9c11bf2be5ae18caeb7f65a695ce45f668e8090a
vidhdhi <vidhdhiyatharans@gmail.com>
2022-10-11 19:18:50 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import BasicButtons from "./components/core/button";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+
+      <div> <BasicButtons/></div>
+    </div>
+  );
+}
+
+export default App;
